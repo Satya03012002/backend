21b287ae69a0bd87ac9770e3298c0e2b7dd94061
satya prakash <satya123.sp34@gmail.com>
2023-04-06 22:36:04 +0530

    back

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/signin",SignInRouter);
+app.use("/",SignInRouter);
 
 
 // if (process.env.NODE_ENV === 'production') {
